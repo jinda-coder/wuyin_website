0b9b5e380dcd36647072eb6596207a213c4641d9
wuyin <2513645274@qq.com>
2026-04-06 22:39:41 +0800

    随笔页基础结构

---
+++ src/pages/notes/index.tsx
@@ -0,0 +1,15 @@
+export const Notes: React.FC = () => {
+    return (
+        <div className="notes-page">
+            <section>
+                <p></p>
+                <h1></h1>
+                <p></p>
+            </section>
+
+            <main className="notes-layout">
+
+            </main>
+        </div>
+    )
+}
